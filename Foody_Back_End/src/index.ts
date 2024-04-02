cced104bb38c657d10d4acc5c41b5e3064a46cb1
ro14hit <rohit1499singh@gmail.com>
2024-04-02 21:15:31 +0530

    Added database connection with uri parameter to 'openUri()' error

--- Foody_Back_End/src/index.ts
+++ Foody_Back_End/src/index.ts
@@ -1,15 +1,23 @@
-import express,{Request, Response} from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv/config';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req:Request,res:Response)=>{
-    res.json({message:"Hello!"})
-})
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello!" });
+});
 
-app.listen(7000,()=>{
-    console.log("Server started on localhost:7000")
-})
+app.listen(7000, () => {
+  console.log("Server started on localhost:7000");
+});
